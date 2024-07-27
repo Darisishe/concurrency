f5855a4933412f6677c8687dacdad90cc2b356e9
Darisishe <konev.sa@phystech.edu>
2024-07-28 02:32:31 +0300

    Optimal Memory Orders in new WaitGroup

--- execution/exe/support/wait_group.hpp
+++ execution/exe/support/wait_group.hpp
@@ -9,27 +9,26 @@ namespace exe::support {
 class WaitGroup {
  public:
   void Add(uint32_t count) {
-    counter_.fetch_add(count);
+    counter_.fetch_add(count, std::memory_order_relaxed);
   }
 
   void Done() {
     auto wake_key = twist::ed::futex::PrepareWake(counter_);
 
-    if (counter_.fetch_sub(1) == 1 && parked_.load() > 0) {
+    if (counter_.fetch_sub(1, std::memory_order_release) == 1 && parked_.load(std::memory_order_relaxed) > 0) {
       twist::ed::futex::WakeAll(wake_key);
     }
   }
 
   void Wait() {
     while (true) {
-      uint32_t curr_counter = counter_.load();
+      uint32_t curr_counter = counter_.load(std::memory_order_acquire);
       if (curr_counter == 0) {
         break;
       }
-      
-      parked_.fetch_add(1);
-      twist::ed::futex::Wait(counter_, curr_counter);
-      parked_.fetch_sub(1);
+      parked_.fetch_add(1, std::memory_order_relaxed);
+      twist::ed::futex::Wait(counter_, curr_counter, std::memory_order_relaxed);
+      parked_.fetch_sub(1, std::memory_order_relaxed);
     }
   }
 
